67c885774c850e660033abf92244124e7b3a0d74
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-21 15:30:22 +0300

    adding structure function to header file

--- header_functions.h
+++ header_functions.h
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * char_func - structure to find functions based on char
+ * @c: char to be checked as key.
+ * @f: pointer to function to call.
+ */
+
+typedef struct char_func
+{
+	char *c;
+	int (*f)();
+} char_funcs;
 
 int _printf(const char *, ...);
 #endif
