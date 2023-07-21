f0c7792f215a853c6151f7b0a9581f889bc08924
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-21 15:21:07 +0300

    adding structure files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "header_functions.h"
+
+/**
+ * _printf - clone for original printf
+ * @format: pointer for format of
+ * arguments
+ * Returns: count of printed characters
+ */
+int _printf(const char *format, ...)
+{
+
+}
---
+++ header_functions.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _printf(const char *, ...);
+#endif
