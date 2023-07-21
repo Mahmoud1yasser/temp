891141aa94ad57c6d88715d077b90a679ce722d4
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-07-21 15:43:14 +0300

    add char_funcs conversion

--- _printf.c
+++ _printf.c
@@ -8,5 +8,23 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list to_be_printed;
+	char_funcs conversions[] = {{"c", print_c},
+		{"s", print_s},
+		{"i", print_int},
+		{"d", print_int},
+		{"u", print_u},
+		{"o", print_o},
+		{"x", print_hex},
+		{"X", print_heX},
+		{"b", print_b},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{"p", print_p},
+		{NULL, NULL},
+	};
+	va_start(to_be_printed, format);
+	va_end(to_be_printed);
 
 }
