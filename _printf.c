7b593750a8d7c03e39b4207c8b2999bda6ae372e
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-07-23 07:21:16 -0700

    fixing beety _printf

--- _printf.c
+++ _printf.c
@@ -58,14 +58,14 @@ int check_con(char formatter, char_funcs con[], char buffer[],
 	return (0);
 }
 /**
- * formatprinter - function search in string format for the %.
+ * formatp - function search in string format for the %.
  * @format: string which contains list of % indentifier.
  * @to_be_printed: arguments in printf
  * @buffer: memory for printing
- * @buflenptr: pointer to buffer length
- * @bufposptr: pointer to current
+ * @ptrblen: pointer to buffer length
+ * @ptrbpos: pointer to current
  * position in buffer
- * @conversions: pointer to format func.
+ * @con: pointer to format func.
  * Return: position of identifier.
  */
 int formatp(const char *format,
